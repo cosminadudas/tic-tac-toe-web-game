ed1c9cfba811535cf131df466ca86eb88cf67b3f
cosminadudas <cosmina.dudas@yahoo.ro>
2022-02-11 22:02:27 +0200

    adds logic for checking for a winner or draw

--- scripts/game.js
+++ scripts/game.js
@@ -33,12 +33,62 @@ function selectGameField(event) {
     selectedField.classList.add('disabled');
     
     // update field that was selected
-    
     gameData[selectedRow][selectedColumn] = activePlayer + 1;
-    console.log(gameData);
+
+    // check for game over
+    const winnerId = checkForGameOver();
 
     // switch to the other symbol
     switchPlayer();
+    currentRound++;
 
-    // keep track of all the selected fields
+}
+
+function checkForGameOver() {
+    // Checking the rows for equality
+    for (let i = 0; i < gameData.length; i++) {
+        if (
+            gameData[i][0] > 0 &&
+            gameData[i][0] === gameData[i][1] &&
+            gameData[i][1] === gameData[i][2]
+        ) {
+            return gameData[i][0];
+        }
+    }
+
+    // Checking columns for equality
+    for (let i = 0; i < gameData.length; i++) {
+        if (
+            gameData[0][i] > 0 &&
+            gameData[0][i] === gameData[1][i] &&
+            gameData[0][i] === gameData[2][i]
+        ) {
+            return gameData[0][i]
+        }
+    }
+
+    // Checking the diagonal: top left to bottom right
+    if (
+        gameData[0][0] > 0 &&
+        gameData[0][0] === gameData[1][1] &&
+        gameData[1][1] === gameData[2][2]
+    ) {
+        return gameData[0][0];
+    }
+
+    // Checking the diagonal: top right to bottom left
+    if (
+        gameData[2][0] > 0 &&
+        gameData[2][0] === gameData[1][1] &&
+        gameData[1][1] === gameData[0][2]
+    ) {
+        return gameData[2][0];
+    }
+
+    if (currentRound == 9) {
+        return -1;
+    }
+
+    return 0;
+
 }
